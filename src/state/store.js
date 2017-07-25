ef16790affdad51b7a01489700d27678f58875d5
Irene <irene.nikkarinen@helsinki.fi>
2017-07-25 13:53:30 +0300

    Partial storing state into localsotrage

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,8 @@
 // @flow
 import { applyMiddleware, createStore, compose } from 'redux';
 import thunk from 'redux-thunk';
-import Api from 'utils/api/index';
+import Api from 'utils/api';
+import { save, load } from 'redux-localstorage-simple';
 import rootReducer from './reducer';
 import { trackLoginStateAction } from './user';
 import { setReviewableExerciseAction } from './review';
@@ -15,10 +16,11 @@ export default function makeStore(assignmentId: string, review: boolean) {
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   /* eslint-enable no-underscore-dangle */
+  const initial = JSON.parse(localStorage.getItem('redux_localstorage_simple'));
   const store = createStore(
-    rootReducer(assignmentId),
+    rootReducer(assignmentId), initial,
     composeEnhancers(
-      applyMiddleware(thunk.withExtraArgument({ api })),
+      applyMiddleware(thunk.withExtraArgument({ api }), save()),
     ),
   );
   store.dispatch(trackLoginStateAction());
